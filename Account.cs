0cbcd79b8e7ed977c8b2053c75ad087c8c1e82f4
CaesarSallad <bruno.kogej@gmail.com>
2026-09-11 13:05:54 +0200

    Added Deposit method

--- Account.cs
+++ Account.cs
@@ -4,5 +4,7 @@ public class Account
 {
     public int Balance {get;set;}
 
-    public string AccountNumber {get; set;}
+    public string AccountNumber {get; set;} = "";
+
+    public void Deposit(int amount) { }
 }
